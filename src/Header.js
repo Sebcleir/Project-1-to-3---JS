1eb58089daad0a942509d90f80069f7d87ce1c85
Nasir Hussain <iamnasir345@gmail.com>
2024-02-07 15:39:50 +0500

    navigation bar

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import "./Header.css";
+import LogoHeader from "./assets/images/logo-header.svg"
+
+function Header() {
+    return (
+        <div className="header">
+            <img style={{marginLeft: "10px"}} src={LogoHeader} alt="logo" />
+            <div className="navigation">
+                <a className="navigation-item" href="#">BACKCHANNEL</a>
+                <a className="navigation-item" href="#">BUSINESS</a>
+                <a className="navigation-item" href="#">CULTURE</a>
+                <a className="navigation-item" href="#">GEAR</a>
+                <a className="navigation-item" href="#">IDEAS</a>
+                <a className="navigation-item" href="#">POLITICS</a>
+                <a className="navigation-item" href="#">SCIENCE</a>
+                <a className="navigation-item" href="#">SECURITY</a>
+                <a className="navigation-item" href="#">MERCH</a>
+            </div>
+            <div className="header-right">
+                <a className="navigation-item" href="#">SIGNIN</a>
+                <button className="navigation-item subscribe">SUBSCRIBE</button>
+                <a href="#">Search</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
